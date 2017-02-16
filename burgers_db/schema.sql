568141ffd11e43de69b81470997f39f30a27b07c
Bob Deckard <bdeckard92@gmail.com>
2017-02-16 17:11:00 -0600

    got my files together... prolly won't work tho..

--- burgers_db/schema.sql
+++ burgers_db/schema.sql
@@ -1,26 +1,13 @@
-/*
 
-To run this file, we do the following in our Terminal:
 
-1. Go to the directory of this sql file.
-
-2. Get into our mysql console.
-
-3. Run "source schema.sql"
-
-*/
-
--- Create the database event_saver_db and specified it for use.
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
-
 CREATE TABLE burgers
 (
-id int NOT NULL AUTO_INCREMENT,
-burger_name varchar(255) NOT NULL,
-devoured ENUM('false', 'true') NOT NULL DEFAULT 'false',
-date_time TIMESTAMP() NOT NULL,
-PRIMARY KEY (id)
+	id int NOT NULL AUTO_INCREMENT,
+	burgername varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	date_time TIMESTAMP,
+	PRIMARY KEY (id)
 );
-
--- server.js
+++ server.js
@@ -1,89 +1,27 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
-var mysql = require("mysql");
 
-var exphbs = require("express-handlebars");
-
-
-var app = express();
 var port = 3000;
 
+var app = express();
 
-app.use(express.static(__dirname + "/public"));
-
+// static content 
+app.use(express.static(process.cwd() + "/public"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
 app.use(methodOverride("_method"));
+
 
 var exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));//left over from movie schedulre chage to view and index?
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Bthootu16",
-  database: "burgers_db"
-});
+var routes = require("./controllers/burgersController.js");
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-
-  console.log("connected as id " + connection.threadId);
-
-});
-
-app.get("/", function(req, res) {
-  connection.query("SELECT * FROM burgers", function(err, data) {
-    if (err) {
-      throw err;
-    }
-
-    res.render("index", { burger: data });
-
-  });
-});
-
-app.post("/", function(req, res) {
-  connection.query("INSERT INTO burgers (burger) VALUES (?)", [req.body], function(err, result) {
-    if (err) {
-      throw err;
-    }
-
-    res.redirect("/");
-  });
-});
-
-app.delete("/:id", function(req, res) {
-  connection.query("DELETE FROM burgers WHERE id = ?", [req.params.id], function(err, result) {
-    if (err) {
-      throw err;
-    }
-
-    res.redirect("/");
-  });
-});
-
-app.put("/", function(req, res) {
-
-  connection.query("UPDATE burgers SET burger = ? WHERE id = ?", [
-    req.body.plan, req.body.id
-  ], function(err, result) {
-    if (err) {
-      throw err;
-    }
-
-    res.redirect("/");
-  });
-});
+app.use("/", routes);
 
 app.listen(port);
